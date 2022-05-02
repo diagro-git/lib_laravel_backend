256dd20c262f48a8d5d9e71b6aa3e5e37e063419
Stijn Leenknegt <stijn@diagro.be>
2022-05-02 11:54:57 +0200

    version 1.9.6

--- src/Traits/DiagroCacheDelete.php
+++ src/Traits/DiagroCacheDelete.php
@@ -18,27 +18,47 @@ trait DiagroCacheDelete
     protected static function bootDiagroCacheDelete()
     {
         static::created(function(Model $model) {
+            logger()->debug("created called for " . get_class($model));
             $dbname = $model->getConnection()->getDatabaseName();
             $table = $model->getTable();
             DeleteResourceCache::dispatchAfterResponse(self::resourceToCacheResourceKey($dbname, $table, '*'));
         });
 
         static::updated(function(Model $model) {
+            logger()->debug("updated called for " . get_class($model));
             $dbname = $model->getConnection()->getDatabaseName();
             $table = $model->getTable();
             $key = $model->getKey();
             if(is_array($key)) {
                 $key = implode('.', $key);
+            } elseif (! is_string($key)) {
+                $key = (string)$key;
             }
             DeleteResourceCache::dispatchAfterResponse(self::resourceToCacheResourceKey($dbname, $table, $key));
         });
 
         static::deleted(function(Model $model) {
+            logger()->debug("deleted called for " . get_class($model));
             $dbname = $model->getConnection()->getDatabaseName();
             $table = $model->getTable();
             $key = $model->getKey();
             if(is_array($key)) {
                 $key = implode('.', $key);
+            } elseif (! is_string($key)) {
+                $key = (string)$key;
+            }
+            DeleteResourceCache::dispatchAfterResponse(self::resourceToCacheResourceKey($dbname, $table, $key));
+        });
+
+        static::restored(function(Model $model) {
+            logger()->debug("restored called for " . get_class($model));
+            $dbname = $model->getConnection()->getDatabaseName();
+            $table = $model->getTable();
+            $key = $model->getKey();
+            if(is_array($key)) {
+                $key = implode('.', $key);
+            } elseif (! is_string($key)) {
+                $key = (string)$key;
             }
             DeleteResourceCache::dispatchAfterResponse(self::resourceToCacheResourceKey($dbname, $table, $key));
         });
