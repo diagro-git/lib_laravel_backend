d9b7d54a849353a56fe9dd94471e515097c2fca7
Stijn Leenknegt <stijn@diagro.be>
2022-07-05 12:13:28 +0200

    diagro commands 10

--- src/Console/Commands/DiagroMigration.php
+++ src/Console/Commands/DiagroMigration.php
@@ -6,11 +6,16 @@ use Illuminate\Database\Console\Migrations\MigrateMakeCommand;
 class DiagroMigration extends MigrateMakeCommand
 {
     /**
-     * The name and signature of the console command.
+     * The console command signature.
      *
      * @var string
      */
-    protected $signature = 'diagro:migration';
+    protected $signature = 'diagro:migration {name : The name of the migration}
+        {--create= : The table to be created}
+        {--table= : The table to migrate}
+        {--path= : The location where the migration file should be created}
+        {--realpath : Indicate any provided migration file paths are pre-resolved absolute paths}
+        {--fullpath : Output the full path of the migration}';
 
     /**
      * The console command description.
