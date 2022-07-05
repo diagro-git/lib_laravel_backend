7cff78c9c651eafde5d45976da24429eee1a8a14
Stijn Leenknegt <stijn@diagro.be>
2022-07-05 12:19:07 +0200

    version 2.7

--- src/Console/Commands/DiagroModel.php
+++ src/Console/Commands/DiagroModel.php
@@ -46,4 +46,23 @@ class DiagroModel extends ModelMakeCommand
             '--model' => $this->qualifyClass($this->getNameInput()),
         ]);
     }
+
+    /**
+     * Create a migration file for the model.
+     *
+     * @return void
+     */
+    protected function createMigration()
+    {
+        $table = Str::snake(Str::pluralStudly(class_basename($this->argument('name'))));
+
+        if ($this->option('pivot')) {
+            $table = Str::singular($table);
+        }
+
+        $this->call('diagro:migration', [
+            'name' => "create_{$table}_table",
+            '--create' => $table,
+        ]);
+    }
 }
